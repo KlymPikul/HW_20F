13f01c5b5181848de0d4dc252fc98c75d91d669c
KlymPikul <klimpikul1989@gmail.com>
2024-03-04 19:08:01 +0100

    change data json file

--- UKRAINE/script.js
+++ UKRAINE/script.js
@@ -5,16 +5,15 @@ const nationalLanguage = document.getElementById("language");
 const countryCapital = document.getElementById("capital");
 const countryAnthem = document.getElementById("anthem");
 const countrySeal = document.getElementById("seal");
-const countryLocation = document.getElementById("location");
 const countryPopulation = document.getElementById("population");
 const nationalCurrency = document.getElementById("currency");
 const countryTimeZone = document.getElementById("timeZone");
 
 const getData = async () => {
   try {
-    const response = await fetch("data.json");
+    const response = await fetch("https://klympikul.github.io/HW_20F/API/data.json");
     const data = await response.json();
-    const { name, flag, found, languages, capital, anthem, seal, location, population, currency, timeZone } = data;
+    const { name, flag, found, languages, capital, anthem, seal, population, currency, timeZone } = data;
 
     filmTitle.textContent = name;
     flagImg.src = flag;
@@ -23,7 +22,6 @@ const getData = async () => {
     countryCapital.textContent = `Capital: ${capital}`;
     countryAnthem.innerHTML = `<a href="${anthem}" target="_blank">Anthem</a>`;
     countrySeal.src = seal;
-    countryLocation.innerHTML = `<a href="${location}" target="_blank">Location</a>`;
     countryPopulation.textContent = `Population: ${population}`;
     nationalCurrency.textContent = `Currency: ${currency.name} (${currency.code})`;
     countryTimeZone.textContent = `Time Zone: ${timeZone}`;
